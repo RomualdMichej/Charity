d5aa656e66e126ca1d39eadcf97f848ce7519db6
RomualdMichej <rmibet@gmail.com>
2020-07-10 14:41:58 +0200

    Zablokawnie edycji darowizn nieupoważnionym użytkownikom 10.07.2020 14:41

--- src/main/java/pl/coderslab/charity/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/SecurityConfig.java
@@ -1,6 +1,5 @@
 package pl.coderslab.charity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,80 +9,12 @@ import org.springframework.security.config.annotation
         .web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import pl.coderslab.charity.seciurytyUser.CustomUserDetailsService;
-//import pl.coderslab.charity.userSeciurity.SpringDataUserDetailsService;
-//import pl.coderslab.charity.seciurityUser.CustomUserDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////    private CustomUserDetailsService customUserDetailsService;
-////
-////    @Autowired
-////    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
-////        this.customUserDetailsService = customUserDetailsService;
-////    }
-////
-////    @Override
-////    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-////
-////        auth.userDetailsService(customUserDetailsService)
-////                .passwordEncoder(passwordEncoder());
-////    }
-//
 
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user1").password("{noop}user123").roles("USER")
-//                .and()
-//                .withUser("admin1").password("{noop}admin123").roles("ADMIN");
-//    }
-//
-////    @Bean
-////    public SpringDataUserDetailsService customUserDetailsService() {
-////        return new SpringDataUserDetailsService();
-////    }
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.authorizeRequests()
-//                .antMatchers("/user/**", "/").permitAll()
-////                .antMatchers("/donation/**").authenticated()
-//                .antMatchers("/donation/**").hasRole("USER")
-//                .and().formLogin().permitAll()
-//                .and().logout().logoutSuccessUrl("/login");
-//
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/login")
-//                .permitAll()
-//                .antMatchers("/donation/**")
-//                .hasAnyRole("ADMIN", "USER")
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-////                .defaultSuccessUrl("/home")
-////                .failureUrl("/login?error=true")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutSuccessUrl("/")
-////                .invalidateHttpSession(true);
-////                .permitAll()
-//                .and()
-//                .csrf()
-//                .disable();
-//    }
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
 
     private CustomUserDetailsService customUserDetailsService;
 
@@ -99,18 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder());
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.authorizeRequests()
-//                .antMatchers("/user/**", "/").permitAll()
-//                .antMatchers("/donation/**").hasAnyAuthority("USER", "ADMIN")
-////                .antMatchers("/authorForm/**").hasRole("USER")
-//                .and().formLogin()
-//                .and().logout().logoutSuccessUrl("/login");
-//
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -124,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 "/user/registerAdmin",
                                 "/category/**",
                                 "/institution/**",
-                                "/donation/show")
+                                "/donation/show",
+                                "/donation/edit")
                 .hasAuthority("ADMIN")
 //                .antMatchers("/donation/**")
 //                .hasAnyAuthority("USER", "ADMIN")
